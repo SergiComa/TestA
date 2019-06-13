42041be924239a15e255e8d5dbe07d15835b67b1
SergiComa <sccorcuera@gmail.com>
2019-06-13 13:03:56 +0200

    TestA

---
+++ PooVueling/PooVueling/Calculadora.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVueling
+{
+    public class Calculadora : ICalculadora, ICloneable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            throw new NotImplementedException("This method is not implemented yet");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public int Division(int num1, int num2)
+        {
+            try
+            {
+                return num1 / num2;
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public int Multiplicacion(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public int Resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public int Suma(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ PooVueling/PooVueling/ICalculadora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVueling
+{
+    public interface ICalculadora
+    {
+        ///<sumary>
+        ///Sumas the specified num1
+        ///</sumary>
+        ///<param name="num1">The num1</param>
+        ///<param name="num2">The num2</param>
+        ///<returns></returns>
+        int Suma(int num1, int num2);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        int Resta(int num1, int num2);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        int Multiplicacion(int num1, int num2);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        int Division(int num1, int num2);
+
+    }
+}
